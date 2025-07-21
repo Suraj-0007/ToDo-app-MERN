646a22ac76c35f43c911cab57d90caacbe2131e6
Suraj-0007 <139526778+Suraj-0007@users.noreply.github.com>
2025-07-21 16:52:11 +0530

    Update server.js

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -16,13 +16,7 @@ const allowedOrigins = [
 const app = express();
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true
   credentials: true
 }));
 

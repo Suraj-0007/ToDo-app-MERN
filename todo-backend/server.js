04d06e0adf0bc1f6f13ff12a4b166f9a7e3ff9dd
Suraj-0007 <139526778+Suraj-0007@users.noreply.github.com>
2025-07-21 16:06:01 +0530

    Update server.js

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // ✅ CORS setup for Vercel frontend
 app.use(cors({
-  origin: ['https://to-do-app-mern-plum.vercel.app'],
+  origin: ['https://to-do-app-mern-c60dukdgh-suraj-0007s-projects.vercel.app/'],
   credentials: true
 }));
 
